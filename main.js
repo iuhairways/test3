9ec7f8cd0e498c50203753003c0c247b5d9c00c7
iuhairways <103064103+iuhairways@users.noreply.github.com>
2022-09-19 15:57:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ async function init(){
     //Load liveries
 
 
-    await fetch("https://gist.githubusercontent.com/iuhairways/11508f80d63f1336e43ca920187903cd/raw/26d138b2d6addac09c00a6d435cb2cb98e0d2e2d/modified%2520liveryjson%2520kolos26").then(res => res.json()).then(data => liveryobj = data)
+    await fetch("https://gist.githubusercontent.com/iuhairways/11508f80d63f1336e43ca920187903cd/raw/5ecd3f48764fe51f9939acf59f797fea97394bf2/modified%2520liveryjson%2520kolos26").then(res => res.json()).then(data => liveryobj = data)
 
 
     //remove original buttons
